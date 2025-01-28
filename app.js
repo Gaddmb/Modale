41b291c67a163a7bf8ad558b5eff4691b954aeca
Gaddmb <gaddielmb@gmail.com>
2025-01-28 16:36:33 +0100

    Last modification

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const focusableSelector = "button, a, input, textarea"; // les éléments sur le
 let focusables = []; // tableau qui contiendra les éléments sur lesquels on peut focus
 
 // cette fonction permet d'ouvrir le modal en cliquant sur le bouton
-const openModal = function (e) {
+const openModal = async function (e) {
   e.preventDefault();
   // peu importe le line que je clique, je veux que le modal s'ouvre donc je vais chercher le href de l'élément sur lequel j'ai cliqué
-  modal = document.querySelector(e.target.getAttribute("href"));
+  const target = e.target.getAttribute("href");
+  if (target.startsWith("#")) {
+    modal = document.querySelector(target);
+  } else {
+    modal = await loadModal(target);
+  }
   // tout les elements sur lesquels on peut focus
   focusables = Array.from(modal.querySelectorAll(focusableSelector));
 
@@ -41,7 +46,7 @@ const closeModal = function (e) {
   modal
     .querySelector(".js-modal-stop")
     .removeEventListener("click", stopPropagation);
-  modal = null;
+
   const hideModal = function () {
     modal.style.display = "none";
     modal.removeEventListener("animationend", hideModal);
@@ -58,6 +63,7 @@ const stopPropagation = function (e) {
 // fonction qui permet de focus sur les éléments du modal
 const focusInmodal = function (e) {
   e.preventDefault();
+  if (modal === null) return;
   // la variable contient la position de l'élément sur lequel on a focus dans le tableau focusables
   let index = focusables.findIndex((f) => f === modal.querySelector(":focus"));
   index++;
@@ -65,6 +71,27 @@ const focusInmodal = function (e) {
     index = 0;
   }
   focusables[index].focus();
+};
+
+// fonction qui permet de charger le modal qui prend en paramètre l'url de la page sur laquelle on se trouve et le # de l'url
+const loadModal = async function (url) {
+  // je recuperer le #de l'url
+  const target = "#" + url.split("#")[1];
+  // verifier si le modal existe déjà
+  const existingModal = document.querySelector(target);
+  // dans ce cas si elle existe je la retourne directement
+  if (existingModal !== null) return existingModal;
+  const html = await fetch(url).then((response) => response.text());
+  console.log(html);
+
+  const element = document
+    .createRange()
+    .createContextualFragment(html)
+    .querySelector(target);
+  if (element === null)
+    throw `L'élément ${target} n'a pas été trouvé dans la page ${url}`;
+  document.body.append(element);
+  return element;
 };
 
 document.querySelectorAll(".js-modal").forEach((a) => {
